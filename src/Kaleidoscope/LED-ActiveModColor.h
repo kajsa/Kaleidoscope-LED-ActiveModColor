ac3527b6e7c85e1b99fc3487689572d51bac3cac
Gergely Nagy <algernon@keyboard.io>
2018-10-09 11:15:55 +0200

    Improve latency by caching interesting keys on layer change
    Keys normally only change when switching layers, so instead of going through
    every key in every cycle to look for modifiers, do that once, when layers
    change (using the new `onLayerChange` event), and store the coordinates of keys
    we consider modifiers in an array (currently limited to 16 items).
    
    Then, when we want to highlight them, go over this array only, significantly
    reducing the work we need to do. In a typical case, on a full-size keyboard, one
    would have eight modifiers and a few layer keys, so instead of going through a
    hundred keys, we go through sixteen at most, but usually considerably less.
    
    This fixes #13, at the cost of noticeably higher PROGMEM and RAM use.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope/LED-ActiveModColor.h
+++ src/Kaleidoscope/LED-ActiveModColor.h
@@ -20,6 +20,8 @@
 #include <Kaleidoscope.h>
 #include <Kaleidoscope-LEDControl.h>
 
+#define MAX_MODS_PER_LAYER 16
+
 namespace kaleidoscope {
 class ActiveModColorEffect : public kaleidoscope::Plugin {
  public:
@@ -29,6 +31,11 @@ class ActiveModColorEffect : public kaleidoscope::Plugin {
   static cRGB sticky_color;
 
   EventHandlerResult beforeReportingState();
+  EventHandlerResult onLayerChange();
+
+ private:
+  static uint8_t mod_keys_[MAX_MODS_PER_LAYER];
+  static uint8_t mod_key_count_;
 };
 }
 
